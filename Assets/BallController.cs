7a051655cf2e826635964fd434cc11696ff7996d
Rowen0114 <70992813+Rowen0114@users.noreply.github.com>
2020-10-21 15:26:51 +0900

    Initial commit

---
+++ Assets/BallController.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class BallController : MonoBehaviour
+{
+    private GameObject scoreText;
+    int GameScore = 0;
+
+    //ボールが見える可能性のあるz軸の最大値
+    private float visiblePosZ = -6.5f;
+
+    //ゲームオーバを表示するテキスト
+    private GameObject gameoverText;
+
+    void OnCollisionEnter(Collision other)
+    {
+
+        if (other.gameObject.tag == "SmallStarTag")
+        {
+            GameScore += 10;
+        }
+        else if (other.gameObject.tag == "LargeStarTag")
+        {
+            GameScore += 20;
+        }
+        else if (other.gameObject.tag == "SmallCloudTag")
+        {
+            GameScore += 30;
+        }
+        else if (other.gameObject.tag == "LargeCloudTag")
+        {
+            GameScore += 50;
+        }
+   
+    }
+
+        // Use this for initialization
+        void Start()
+    {
+        //シーン中のGameOverTextオブジェクトを取得
+        this.gameoverText = GameObject.Find("GameOverText");
+        this.scoreText = GameObject.Find("ScoreText");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.scoreText.GetComponent<Text>().text = "Score:" + GameScore;
+
+        //ボールが画面外に出た場合
+        if (this.transform.position.z < this.visiblePosZ)
+        {
+            //GameoverTextにゲームオーバを表示
+            this.gameoverText.GetComponent<Text>().text = "Game Over";
+           
+        }
+    }
+}
